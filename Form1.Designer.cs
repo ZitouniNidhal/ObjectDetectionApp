8795b2eacbedecf1bc94d19a63a19c1b936e80d4
ZITOUNI NIDHAL <nidhalzitouni111@gmail.com>
2024-11-22 10:55:56 +0100

    updqte file python

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,95 +1,77 @@
 ﻿using Emgu.CV;
 
-namespace ObjectDetectionApp;
-
-partial class Form1 : System.Windows.Forms.Form
+namespace ObjectDetectionApp
 {
-    private void Form1_FormClosing(object sender, System.Windows.Forms.FormClosingEventArgs e)
+    partial class Form1
     {
-        // Add any cleanup code here if needed
-    }
-    /// <summary>
-    ///  Required designer variable.
-    /// </summary>
-    private System.ComponentModel.IContainer components = null;
+        /// <summary>
+        /// Variable requise par le concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
 
-    private Emgu.CV.VideoCapture capture;
-    private System.Windows.Forms.PictureBox pictureBox;
-    private System.Windows.Forms.Label labelPrediction;
-
-    /// <summary>
-    ///  Clean up any resources being used.
-    /// </summary>
-    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-    protected override void Dispose(bool disposing)
-{
-    if (disposing)
-    {
-        // Libération des ressources managées
-        if (components != null)
+        /// <summary>
+        /// Nettoyer toutes les ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
         {
-            components.Dispose();
+            if (disposing)
+            {
+                if (components != null)
+                {
+                    components.Dispose();
+                }
+            }
+
+            // Libérer les ressources non managées
+            if (capture != null)
+            {
+                capture.Release();
+            }
+            CvInvoke.DestroyAllWindows();
+            base.Dispose(disposing);
+        }
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.labelPrediction = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(640, 480);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+
+            // 
+            // labelPrediction
+            // 
+            this.labelPrediction.Location = new System.Drawing.Point(12, 500);
+            this.labelPrediction.Name = "labelPrediction";
+            this.labelPrediction.Size = new System.Drawing.Size(640, 50);
+            this.labelPrediction.TabIndex = 1;
+            this.labelPrediction.Text = "Résultats de détection";
+
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(664, 561);
+            this.Controls.Add(this.labelPrediction);
+            this.Controls.Add(this.pictureBox);
+            this.Name = "Form1";
+            this.Text = "Application de Détection d'Objets";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
         }
     }
-
-    // Libération des ressources non managées
-    if (capture != null)
-    {
-        capture.Release(); // Libérer la caméra si elle est utilisée
-    }
-    CvInvoke.DestroyAllWindows(); // Fermer les fenêtres OpenCV si elles sont ouvertes
-
-    // Appeler la méthode Dispose de la classe de base
-    base.Dispose(disposing);
-}
-
-
-    #region Windows Form Designer generated code
-
-    /// <summary>
-    ///  Required method for Designer support - do not modify
-    ///  the contents of this method with the code editor.
-    /// </summary>
-
-   private void InitializeComponent()
-{
-    this.pictureBox = new System.Windows.Forms.PictureBox();
-    this.labelPrediction = new System.Windows.Forms.Label();
-    ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
-    this.SuspendLayout();
-
-    // 
-    // pictureBox
-    // 
-    this.pictureBox.Location = new System.Drawing.Point(12, 12);
-    this.pictureBox.Name = "pictureBox";
-    this.pictureBox.Size = new System.Drawing.Size(640, 480);
-    this.pictureBox.TabIndex = 0;
-    this.pictureBox.TabStop = false;
-
-    // 
-    // labelPrediction
-    // 
-    this.labelPrediction.Location = new System.Drawing.Point(12, 500);
-    this.labelPrediction.Name = "labelPrediction";
-    this.labelPrediction.Size = new System.Drawing.Size(640, 50);
-    this.labelPrediction.TabIndex = 1;
-    this.labelPrediction.Text = "Résultats de détection";
-
-    // 
-    // Form1
-    // 
-    this.ClientSize = new System.Drawing.Size(664, 561);
-    this.Controls.Add(this.labelPrediction);
-    this.Controls.Add(this.pictureBox);
-    this.Name = "Form1";
-    this.Text = "Application de Détection d'Objets";
-    this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
-    this.Load += new System.EventHandler(this.Form1_Load);
-    ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
-    this.ResumeLayout(false);
-}
-
-
-    #endregion
 }
--- Form1.cs
+++ Form1.cs
@@ -4,72 +4,103 @@ using Emgu.CV.Structure;
 using System;
 using System.Drawing;
 using System.Net.Http;
+using System.Drawing.Imaging;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-public partial class Form1 : Form
+namespace ObjectDetectionApp
 {
-    private VideoCapture capture;
-    private Mat frame;
-    private static readonly HttpClient client = new HttpClient();
-    private PictureBox pictureBox;
-    
-    public Form1()
+    public partial class Form1 : Form
     {
-        pictureBox = new PictureBox
-        {
-            Dock = DockStyle.Fill,
-            SizeMode = PictureBoxSizeMode.StretchImage
-        };
-        Controls.Add(pictureBox);
-        capture = new VideoCapture(0); // Capture vidéo à partir de la webcam
-        capture = new VideoCapture(0); // Capture vidéo à partir de la webcam
-        frame = new Mat();
-    }
+        private VideoCapture capture;
+        private Mat frame;
+        private static readonly HttpClient client = new HttpClient();
+        private PictureBox pictureBox;
+        private Label labelPrediction;
 
-    // Initialisation au chargement du formulaire
-    private void Form1_Load(object sender, EventArgs e)
-    {
-        System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
-        timer.Interval = 30; // Mise à jour toutes les 30ms (30 FPS)
-        timer.Tick += Timer_Tick;
-        timer.Start();
-    }
-
-    // Méthode pour capturer et afficher l'image
-    private void Timer_Tick(object sender, EventArgs e)
-    {
-        capture.Read(frame); // Lire une frame de la vidéo
-        if (!frame.IsEmpty)
+        public Form1()
         {
-            pictureBox.Image = frame.ToImage<Bgr, byte>().ToBitmap(); // Afficher dans un PictureBox
-            Task.Run(() => PredictObjectAsync(frame)); // Lancer la prédiction dans un thread séparé
+            InitializeComponent(); // Appel du constructeur généré par le designer
+
+            // Initialisation des composants du formulaire
+            pictureBox = new PictureBox
+            {
+                Dock = DockStyle.Fill,
+                SizeMode = PictureBoxSizeMode.StretchImage
+            };
+            labelPrediction = new Label
+            {
+                Dock = DockStyle.Bottom,
+                AutoSize = true
+            };
+
+            Controls.Add(pictureBox);
+            Controls.Add(labelPrediction);
+
+            capture = new VideoCapture(0); // Capture vidéo depuis la webcam
+            frame = new Mat();
+        }
+
+        // Méthode de chargement du formulaire
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
+            timer.Interval = 30; // Mise à jour toutes les 30ms (environ 30 FPS)
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+public Bitmap MatToBitmap(Mat mat)
+{
+    if (mat.IsEmpty)
+        return null;
+
+    // Créer un Bitmap à partir des données du Mat
+    int width = mat.Width;
+    int height = mat.Height;
+    Bitmap bitmap = new Bitmap(width, height, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+
+    // Copier les données de l'image Mat vers le Bitmap
+    var rect = new Rectangle(0, 0, width, height);
+    BitmapData bmpData = bitmap.LockBits(rect, ImageLockMode.WriteOnly, bitmap.PixelFormat);
+    CvInvoke.cvCopy(mat, bmpData.Scan0, IntPtr.Zero);  // Copier les données de l'image dans le Bitmap
+
+    bitmap.UnlockBits(bmpData);
+    return bitmap;
+}
+        // Capture et affichage des frames à chaque "tick" du timer
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            capture.Read(frame); // Capture une frame de la vidéo
+            if (!frame.IsEmpty)
+            {
+                pictureBox.Image = MatToBitmap(frame); // Afficher l'image dans PictureBox
+                Task.Run(() => PredictObjectAsync(frame)); // Lancer la prédiction dans un thread séparé
+            }
+        }
+
+        // Envoi de l'image à l'API Python pour prédiction d'objets
+        private async Task PredictObjectAsync(Mat frame)
+        {
+            var imageBytes = frame.ToImage<Bgr, byte>().ToJpegData();
+            var imageContent = new ByteArrayContent(imageBytes);
+            var formData = new MultipartFormDataContent();
+            formData.Add(imageContent, "image", "frame.jpg");
+
+            var response = await client.PostAsync("http://localhost:5000/predict", formData);
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            // Mise à jour de l'interface utilisateur avec les résultats de la détection
+            Invoke(new Action(() =>
+            {
+                labelPrediction.Text = responseString; // Affichage des résultats dans le Label
+            }));
+        }
+
+        // Libération des ressources à la fermeture du formulaire
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            capture.Release(); // Libérer la caméra
+            CvInvoke.DestroyAllWindows(); // Fermer toutes les fenêtres OpenCV
         }
     }
-
-    // Méthode pour envoyer l'image à l'API Python pour la détection d'objets
-    private async Task PredictObjectAsync(Mat frame)
-    {
-        var imageBytes = frame.ToImage<Bgr, byte>().ToJpegData();
-        var imageContent = new ByteArrayContent(imageBytes);
-        var formData = new MultipartFormDataContent();
-        formData.Add(imageContent, "image", "frame.jpg");
-
-        var response = await client.PostAsync("http://localhost:5000/predict", formData);
-        var responseString = await response.Content.ReadAsStringAsync();
-
-        // Traiter les résultats de la détection (par exemple, afficher les objets détectés)
-        Invoke(new Action(() =>
-        {
-            labelPrediction.Text = responseString; // Afficher les résultats dans un label
-        }));
-    }
-
-    // Libération des ressources à la fermeture
-  private void Form1_FormClosing(object sender, FormClosingEventArgs e)
-{
-    capture.Release(); // Libérer la caméra
-    CvInvoke.DestroyAllWindows(); // Fermer les fenêtres OpenCV
-}
-
 }
